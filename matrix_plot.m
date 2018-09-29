f0b86027e93177f3ff5fe87cf8813af2dc2c54e3
yuetianluo <luoyuetian123@ruc.edu.cn>
2018-09-29 16:07:16 -0500

    update matrix plot

--- matrix_plot.m
+++ matrix_plot.m
@@ -1,4 +1,4 @@
-function matrix_plot(matrix, d2, d3, color_map)
+function matrix_plot(matrix, color_map)
 %%%%%%%%%%%%%%%%%%%%
 % Authors: Yuetian Luo (yluo86@wisc.edu), Anru Zhang (anruzhang@stat.wisc.edu)
 % University of Wisconsin-Madison
@@ -6,37 +6,71 @@ function matrix_plot(matrix, d2, d3, color_map)
 % Plot a matrix in 3-d dimension
 % MATRIX: defines the matrix input, and the scalar value in the matrix
 % represent the color depth in the "jet" colormap
-% d2: a scalar value 0 or 1. 1 means plot the 2-d matrix, 0 means don't
-% plot the 2-d matrix
-% d3: a scalar value 0 or 1. 1 means plot the 3-d matrix, 0 means don't
-% plot the 3-d matrix.
 % COLOR_MAP: A input of the color map you want to visualize your plot. For
 % different color maps, you can see
-% "https://www.mathworks.com/help/matlab/ref/colormap.html"
+% "https://www.mathworks.com/help/matlab/ref/colormap.html". Also you can
+% use 'green', 'blue', 'red' colormap.
     [nrow, ncol] = size(matrix);
-    col_add = zeros(nrow, 1);
-    matrix = [matrix, col_add];
-    [nrow, ncol] = size(matrix);
-    row_add = zeros(1, ncol);
-    matrix = vertcat(matrix, row_add);
-    if (d2 == 1 & d3 ==1)
-    figure;
-    subplot(1,2,1);
-    pcolor(matrix);
-    colormap(color_map);
-    subplot(1,2,2);
-    pcolor(matrix);
-    view(3)
-    axis off 
-    elseif (d2 == 0 & d3 ==1)
-        figure(1);
-        pcolor(matrix);
-        colormap(color_map)
-        view(3)
-        axis off 
-    elseif (d2 == 1 & d3 ==0)
-        figure(1);
-        pcolor(matrix);
+    if strcmp(color_map, 'green')
+        matrix = 1-abs(matrix)/(max(abs(matrix(:))));
+        mymap = [0,1,0;
+            0.1,1,0.1;
+           0.2,1,0.2;
+           0.3,1,0.3;
+           0.4,1,0.4;
+           0.5,1,0.5;
+           0.6,1,0.6;
+           0.7,1,0.7;
+           0.8,1,0.8;
+           0.9,1,0.9
+           1,1,1];
+        imagesc(matrix);
+        colormap(mymap);
+    end
+    if strcmp(color_map, 'blue')
+        matrix = 1-abs(matrix)/(max(abs(matrix(:))));
+        mymap = [0,0,1;
+            0.1,0.1,1;
+           0.2,0.2,1;
+           0.3,0.3,1;
+           0.4,0.4,1;
+           0.5,0.5,1;
+           0.6,0.6,1;
+           0.7,0.7,1;
+           0.8,0.8,1;
+           0.9,0.9,1;
+           1,1,1];
+        imagesc(matrix);
+        colormap(mymap);
+    end
+    if strcmp(color_map, 'red')
+        matrix = 1-abs(matrix)/(max(abs(matrix(:))));
+        mymap = [1,0,0;
+            1,0.1,0.1;
+           1,0.2,0.2;
+           1,0.3,0.3;
+           1,0.4,0.4;
+           1,0.5,0.5;
+           1,0.6,0.6;
+           1,0.7,0.7;
+           1,0.8,0.8;
+           1,0.9,0.9;
+           1,1,1];
+        imagesc(matrix);
+        colormap(mymap);
+    end
+    if ~strcmp(color_map, 'green') & ~strcmp(color_map,'blue') & ~strcmp(color_map,'red')
+        matrix = 1-abs(matrix)/(max(abs(matrix(:))));
+        imagesc(matrix);
         colormap(color_map);
+    end
+    axis off;
+    hold on;
+    for i = 1:(nrow+1)
+        plot([0.5, ncol + 0.5], [i-0.5, i-0.5], 'k-');
+    end
+    hold on;
+    for j = 1:(ncol+1)
+        plot([j-0.5,j-0.5], [0.5, nrow + 0.5], 'k-');
     end
 end
